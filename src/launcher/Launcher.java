b4ce162c70d6b5301a2d5971c68ffded452e7173
Manerial <avr1l.dunmare@gmail.com>
2019-03-25 22:15:00 +0100

    Adding Title example of usage

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -6,6 +6,8 @@ import persona.Persona;
 import persona.PersonaCreator;
 import persona.Physical;
 import persona.PhysicalCreator;
+import persona.Title;
+import persona.TitleCreator;
 import utilities.FileAccess;
 import utilities.Interface;
 import utilities.ItemGenerator;
@@ -23,7 +25,8 @@ public class Launcher {
 		ItemGenerator.character();
 		ItemGenerator.writingOption();
 		Physical phys1 = PhysicalCreator.createPhysical("Bleus", "Courts bouclés", null, null, 150, 80000, null, null, -1, FileAccess.FR_DIR);
-		Persona p1 = PersonaCreator.createPersona(1, 82, "Zorora", "Miss Terre", null, null, phys1, null, FileAccess.FR_DIR);
+		Title t1 = TitleCreator.createTitle(null, "de l'or", null, FileAccess.FR_DIR);
+		Persona p1 = PersonaCreator.createPersona(1, 82, "Zorora", "Miss Terre", null, null, phys1, t1, FileAccess.FR_DIR);
 		Interface.display(p1);
 		ArrayList<String> styles = new ArrayList<>();
 		styles.add("Fantaisie");
