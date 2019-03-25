955b9b860183cc2c8163758d97aeb4f3daad9865
Manerial <avr1l.dunmare@gmail.com>
2019-03-25 22:12:05 +0100

    Adding Title creation

---
+++ src/persona/Title.java
@@ -0,0 +1,62 @@
+package persona;
+
+public class Title {
+	private String prefix;
+	private String suffix;
+	private String adjective;
+
+	/**
+	 * Generic constructor
+	 *
+	 * @param prefix : The first part of the title (should not be empty)
+	 * @param suffix : The second part of the title (can be empty)
+	 * @param adjective : The third part of the title (can be empty)
+	 */
+	public Title(String prefix, String suffix, String adjective) {
+		this.prefix = prefix;
+		this.suffix = suffix;
+		this.adjective = adjective;
+	}
+
+	/**
+	 * Empty constructor
+	 */
+	public Title() {
+	}
+
+	/**
+	 * Get method for the title
+	 *
+	 * @return the title
+	 */
+	public String getTitle() {
+		return prefix + ((suffix != null) ? " " + suffix : "") + ((adjective != null) ? " " + adjective : "");
+	}
+
+	/**
+	 * Set method for the prefix
+	 *
+	 * @param prefix : The prefix to set
+	 */
+	public void setName(String prefix) {
+		this.prefix = prefix;
+	}
+
+	/**
+	 * Set method for the suffix
+	 *
+	 * @param suffix : The suffix to set
+	 */
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+	/**
+	 * Set method for the adjective
+	 *
+	 * @param adjective : The adjective to set
+	 */
+	public void setAdjective(String adjective) {
+		this.adjective = adjective;
+	}
+}
---
+++ src/persona/TitleCreator.java
@@ -0,0 +1,73 @@
+package persona;
+
+import java.util.Random;
+
+import utilities.FileAccess;
+import utilities.FileLoader;
+
+public class TitleCreator {
+	private static Random random = new Random();
+
+	/**
+	 * Create a character title with the different option you want.
+	 * If an option is null, it will be generate randomly.
+	 *
+	 * @param prefix : If null, created randomly
+	 * @param suffix : If null, created randomly
+	 * @param adjective : If null, created randomly
+	 * @param language : The language you want to use for the creation. Uses the one of FileAccess.
+	 * @return
+	 */
+	public static Title createTitle(String prefix, String suffix, String adjective, String language) {
+		prefix = getPrefix(prefix, language);
+		suffix = getSuffix(suffix, language);
+		adjective = getAdjective(adjective, language);
+		return new Title(prefix, suffix, adjective);
+	}
+
+	/**
+	 * If needed, get a random prefix.
+	 *
+	 * @param prefix : If null, generate a new one
+	 * @param language : The generation language
+	 * @return the prefix
+	 */
+	private static String getPrefix(String prefix, String language) {
+		if (prefix == null) {
+			prefix = FileLoader.getRandomLine(FileAccess.RESSOURCES + language + FileAccess.TIT_PREFIX);
+		}
+		return prefix;
+	}
+
+	/**
+	 * If needed, get a random suffix.
+	 * There is 3 chances out of 5 to generate one.
+	 *
+	 * @param suffix : If null, generate a new one
+	 * @param language : The generation language
+	 * @return the suffix
+	 */
+	private static String getSuffix(String suffix, String language) {
+		// 3 chances out of 5
+		if (suffix == null && random.nextInt(5) < 3) {
+			suffix = FileLoader.getRandomLine(FileAccess.RESSOURCES + language + FileAccess.TIT_SUFFIX);
+		}
+		return suffix;
+	}
+
+	/**
+	 * If needed, get a random adjective.
+	 * There is 3 chances out of 5 to generate one.
+	 *
+	 * @param adjective : If null, generate a new one
+	 * @param language : The generation language
+	 * @return the adjective
+	 */
+	private static String getAdjective(String adjective, String language) {
+		// 3 chances out of 5
+		if (adjective == null && random.nextInt(5) < 3) {
+			adjective = FileLoader.getRandomLine(FileAccess.RESSOURCES + language + FileAccess.TIT_ADJECTIF);
+		}
+		return adjective;
+	}
+}
