53e206541833f55889263b81ce8e4c2106563601
Manerial <avr1l.dunmare@gmail.com>
2019-03-25 22:13:35 +0100

    Adding Title to description set

--- src/utilities/ItemGenerator.java
+++ src/utilities/ItemGenerator.java
@@ -10,7 +10,7 @@ public class ItemGenerator {
 	 * Generates random character
 	 */
 	public static void character() {
-		Persona persona = PersonaCreator.createPersona(0, 100, null, null, null, null, null, FileAccess.FR_DIR);
+		Persona persona = PersonaCreator.createPersona(0, 100, null, null, null, null, null, null, FileAccess.FR_DIR);
 		Interface.display(persona);
 	}
 
